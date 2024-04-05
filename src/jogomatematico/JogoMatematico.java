4926d28a637924b6d3945e15985ae44d0d272e5b
nattmota <natanyujikawasaki@gmail.com>
2024-04-05 17:03:47 -0300

    commit 2

--- src/jogomatematico/JogoMatematico.java
+++ src/jogomatematico/JogoMatematico.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 public class JogoMatematico {
 
     public static void main(String[] args) {
-
-        boolean resposta = false;
+      
         int tentativas = 5;
         int pontos = 0;
         int rodadas = 0;
@@ -24,8 +23,8 @@ public class JogoMatematico {
         if (userStart.equals("OK") || userStart.equals("ok")) {
             while (rodadas >= 0 && rodadas < 10) {
                 if (tentativas > 0) {
-                    int num = rand.nextInt(10);
-                    int num2 = rand.nextInt(10);
+                    int num = rand.nextInt(20);
+                    int num2 = rand.nextInt(20);
                     int resultado = num + num2;
                     System.out.print(num + "+" + num2 + ": ");
                     userRes = sc.nextInt();
@@ -34,7 +33,7 @@ public class JogoMatematico {
                         System.out.println("Parabéns resposta correta \nPontos: " + pontos);
                     } else {
                         tentativas--;
-                        System.out.println("Resposta incorreta! \nTentativas: " + tentativas);
+                        System.out.println("Resposta incorreta! Resposta correta: " + resultado + "\nTentativas: " + tentativas);
                     }
                     rodadas++;
                     System.out.println(rodadas);
@@ -42,8 +41,8 @@ public class JogoMatematico {
             }
             while (rodadas >= 10 && rodadas < 20) {
                 if (tentativas > 0) {
-                    int num = rand.nextInt(10);
-                    int num2 = rand.nextInt(10);
+                    int num = rand.nextInt(20);
+                    int num2 = rand.nextInt(20);
                     int resultado = num - num2;
                     System.out.print(num + "-" + num2 + ": ");
                     userRes = sc.nextInt();
@@ -52,13 +51,49 @@ public class JogoMatematico {
                         System.out.println("Parabéns resposta correta \nPontos: " + pontos);
                     } else {
                         tentativas--;
-                        System.out.println("Resposta incorreta! \nTentativas: " + tentativas);
+                        System.out.println("Resposta incorreta! Resposta correta: " + resultado + "\nTentativas: " + tentativas);
                     }
                     rodadas++;
                     System.out.println(rodadas);
                 }
             }
-
+            while (rodadas >= 20 && rodadas < 30) {
+                if (tentativas > 0) {
+                    int num = rand.nextInt(10);
+                    int num2 = rand.nextInt(10);
+                    int resultado = num * num2;
+                    System.out.print(num + "*" + num2 + ": ");
+                    userRes = sc.nextInt();
+                    if (userRes == resultado) {
+                        pontos += 5;
+                        System.out.println("Parabéns resposta correta \nPontos: " + pontos);
+                    } else {
+                        tentativas--;
+                        System.out.println("Resposta incorreta! Resposta correta: " + resultado + "\nTentativas: " + tentativas);
+                    }
+                    rodadas++;
+                    System.out.println(rodadas);
+                }
+            }
+            while (rodadas >= 30 && rodadas < 36) {
+                if (tentativas > 0) {
+                    int num = rand.nextInt(10);
+                    int num2 = rand.nextInt(10);
+                    int resultado = num / num2;
+                    System.out.print(num + "/" + num2 + ": ");
+                    userRes = sc.nextInt();
+                    if (userRes == resultado) {
+                        pontos += 5;
+                        System.out.println("Parabéns resposta correta \nPontos: " + pontos);
+                    } else {
+                        tentativas--;
+                        System.out.println("Resposta incorreta! Resposta correta: " + resultado + "\nTentativas: " + tentativas);
+                    }
+                    rodadas++;
+                    System.out.println(rodadas);
+                }
+            }
+            
             System.out.println("Pontos ganhos: " + pontos);
 
         } else {
